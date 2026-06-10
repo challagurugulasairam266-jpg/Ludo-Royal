9934ac9c573a81aac4cb24897e2c06d587f9ac8c
challagurugulasairam266-jpg <challagurugulasairam266@gmail.com>
2026-06-11 01:58:53 +0530

    Add button component

---
+++ app/button.tsx
@@ -0,0 +1,15 @@
+export default function Button() {
+  return (
+    <button style={{
+      padding:'15px 30px', 
+      fontSize:'18px', 
+      borderRadius:'10px', 
+      border:'none', 
+      background:'#4f46e5', 
+      color:'white',
+      cursor:'pointer'
+    }}>
+      Roll Dice
+    </button>
+  )
+}
