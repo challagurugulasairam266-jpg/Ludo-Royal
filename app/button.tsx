e6fa8885736457acdfa0b08b1d745d1870dd2d7f
challagurugulasairam266-jpg <challagurugulasairam266@gmail.com>
2026-06-11 12:20:03 +0530

    Add button type

--- app/button.tsx
+++ app/button.tsx
@@ -1,19 +1,7 @@
-export default function Button({onClick}) {
-  return (
-    <button 
-      onClick={onClick}
-      style={{
-        padding:'14px 28px', 
-        fontSize:'18px',
-        background:'white',
-        color:'black',
-        border:'none',
-        borderRadius:'12px',
-        cursor:'pointer',
-        fontWeight:'bold'
-      }}
-    >
-      Roll Dice
-    </button>
-  )
+type ButtonProps = {
+  onClick: () => void
+}
+
+export default function Button({onClick}: ButtonProps) {
+  return <button onClick={onClick}>Roll Dice</button>
 }
